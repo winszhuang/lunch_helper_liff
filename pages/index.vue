5a140f35dfed0dc095d4ec64370486a1bc168443
wins <win4709hi@gmail.com>
2023-06-18 00:38:34 +0800

    feat: page setting

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      index
+    </div>
+</template>
+
+<script setup lang="ts">
+const { $liffInit, $liff } = useNuxtApp()
+
+onMounted(() => {
+    $liffInit()
+        .then(() => {
+        console.log('近來瞜');
+        console.log("當前版本是: ", $liff().getVersion());
+        })
+    })
+
+</script>
+  
