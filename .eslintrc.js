421ae89c91dd5d7c765e3c5fbbcfdd263865f2cd
wins <win4709hi@gmail.com>
2023-06-18 00:46:33 +0800

    feat: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:vue/vue3-essential'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+    'vue/multi-word-component-names': 0,
+    'vue/no-v-model-argument': 0,
+    'no-undef': 0,
+    'no-unused-vars': 0,
+    'vue/require-default-prop': 0
+  }
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,18 @@
 <template>
-    <div>
-      index
-    </div>
+  <div>
+    index
+  </div>
 </template>
 
 <script setup lang="ts">
 const { $liffInit, $liff } = useNuxtApp()
 
 onMounted(() => {
-    $liffInit()
-        .then(() => {
-        console.log('近來瞜');
-        console.log("當前版本是: ", $liff().getVersion());
-        })
+  $liffInit()
+    .then(() => {
+      console.log('近來瞜')
+      console.log('當前版本是: ', $liff().getVersion())
     })
+})
 
 </script>
-  
